c4ba5d494c5817c0f9c03a589c0eb464ecc53866
Stefan Seeland <stefan.seeland@rubicon.eu>
2021-03-09 19:11:16 +0100

    ExportImageHandler implemented

--- OpenXmlToHtml/OpenXmlToHtml.cs
+++ OpenXmlToHtml/OpenXmlToHtml.cs
@@ -2,6 +2,7 @@
 using OpenXmlPowerTools;
 using OpenXmlPowerTools.OpenXMLWordprocessingMLToHtmlConverter;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -55,10 +56,27 @@ namespace Codeuctivity.OpenXmlToHtml
                 throw new ArgumentNullException(nameof(sourceOpenXml));
             }
 
-            return ConvertToHtmlInternalAsync(sourceOpenXml, fallbackPageTitle);
+            return ConvertToHtmlInternalAsync(sourceOpenXml, fallbackPageTitle, new ImageHandler());
         }
 
-        private static async Task<Stream> ConvertToHtmlInternalAsync(Stream sourceOpenXml, string fallbackPageTitle)
+        /// <summary>
+        /// Converts docx to html
+        /// </summary>
+        /// <param name="sourceOpenXml"></param>
+        /// <param name="fallbackPageTitle"></param>
+        /// <param name="images"></param>
+        /// <returns>selfContainedHtml</returns>
+        public static Task<Stream> ConvertToHtmlAsync(Stream sourceOpenXml, string fallbackPageTitle, IDictionary<string, byte[]> images)
+        {
+            if (sourceOpenXml == null)
+            {
+                throw new ArgumentNullException(nameof(sourceOpenXml));
+            }
+
+            return ConvertToHtmlInternalAsync(sourceOpenXml, fallbackPageTitle, new ExportImageHandler(images));
+        }
+
+        private static async Task<Stream> ConvertToHtmlInternalAsync(Stream sourceOpenXml, string fallbackPageTitle, IImageHandler imageHandler)
         {
             using var memoryStream = new MemoryStream();
             await sourceOpenXml.CopyToAsync(memoryStream).ConfigureAwait(false);
@@ -69,7 +87,7 @@ namespace Codeuctivity.OpenXmlToHtml
             var computedPageTitle = coreFilePropertiesPart?.GetXDocument().Descendants(DC.title).FirstOrDefault();
             var pageTitle = string.IsNullOrEmpty(computedPageTitle?.Value) ? fallbackPageTitle : computedPageTitle!.Value;
 
-            var htmlElement = WmlToHtmlConverter.ConvertToHtml(wordProcessingDocument, CreateHtmlConverterSettings(pageTitle));
+            var htmlElement = WmlToHtmlConverter.ConvertToHtml(wordProcessingDocument, CreateHtmlConverterSettings(pageTitle, imageHandler));
 
             var memoryStreamHtml = new MemoryStream();
             htmlElement.Save(memoryStreamHtml);
@@ -77,9 +95,9 @@ namespace Codeuctivity.OpenXmlToHtml
             return memoryStreamHtml;
         }
 
-        private static WmlToHtmlConverterSettings CreateHtmlConverterSettings(string pageTitle)
+        private static WmlToHtmlConverterSettings CreateHtmlConverterSettings(string pageTitle, IImageHandler imageHandler)
         {
-            var settings = new WmlToHtmlConverterSettings(pageTitle, new ImageHandler(), new TextSymbolToUnicodeHandler(), new SymbolHandler(), new PageBreakHandler(new BreakHandler()), true, string.Empty, "@page { size: A4 } body { margin: 1cm auto; max-width: 20cm; padding: 0; }", "Codeuctivity-");
+            var settings = new WmlToHtmlConverterSettings(pageTitle, imageHandler, new TextSymbolToUnicodeHandler(), new SymbolHandler(), new PageBreakHandler(new BreakHandler()), true, string.Empty, "@page { size: A4 } body { margin: 1cm auto; max-width: 20cm; padding: 0; }", "Codeuctivity-");
 
             return settings;
         }
