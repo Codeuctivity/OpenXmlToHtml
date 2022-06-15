5d19a42e58f3e22b13d49151031942c72e2e8b88
Stefan Seeland <stefan.seeland@rubicon.eu>
2022-06-15 15:58:25 +0200

    Added hack for azure for now... need to migrate to cusom app service containers

--- OpenXmlToHtmlOpenApi/Program.cs
+++ OpenXmlToHtmlOpenApi/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace OpenXmlToHtmlOpenApi
 {
@@ -14,7 +17,28 @@ namespace OpenXmlToHtmlOpenApi
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
+            if (IsRunningOnAzureLinux())
+                SetupChromeiumDependencies();
+
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void SetupChromeiumDependencies()
+        {
+            var azureLinuxAppChromeDependencies = "export DEBIAN_FRONTEND=noninteractive && bash -c 'apt update &&apt upgrade -y apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libasound2 libnss3 -y >/dev/null 2>&1 & disown'";
+
+            Process.Start(azureLinuxAppChromeDependencies);
+        }
+
+        private static bool IsRunningOnAzureLinux()
+        {
+            var websiteSku = Environment.GetEnvironmentVariable("WEBSITE_SKU");
+
+            if (string.IsNullOrEmpty(websiteSku))
+            {
+                return false;
+            }
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && websiteSku.Contains("Linux", StringComparison.InvariantCultureIgnoreCase);
         }
 
         /// <summary>
